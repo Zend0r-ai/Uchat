35f33d82b6d708545abf2f68e5ffc51126656220
EasyWin6 <sunshineregi888@gmail.com>
2020-07-08 16:31:50 +0300

    11

---
+++ blevishche/cmake-build-debug/CMakeFiles/Uchat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Uchat.dir/client/src/client.c.o"
+  "CMakeFiles/Uchat.dir/server/src/main.c.o"
+  "Uchat"
+  "Uchat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Uchat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ blevishche/server/inc/server.h
@@ -0,0 +1,33 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/errno.h>
+#include <pthread.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <sys/un.h>
+#include <sys/event.h>
+#include <fcntl.h>
+
+struct sockaddr_in addr;
+
+//int mx_socket_handler(int client_sock);
+int interact(int client_sock);
+
+
+
+
+
+
+
+
+
+#endif
